446e859252980ecb7f9a345d014d218d056641bf
Kevin del Cid <danieldelcidf@gmail.com>
2022-05-28 12:38:58 -0600

    ejercicio probando el uso de scaffold floatingActionButton, Drawers y backgrounColor

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-            // child: Image.network(
-            //   'https://steamuserimages-a.akamaihd.net/ugc/1689400601813337329/77493C8B648A8DCDE0A7FF49EE4E492A8D79E692/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
-            //   height: 700,
-            //   //aqui podria ponerle un fit: BoxFit.cover; fil ... y usar el width:
-            // ),
-
-            child: IconButton(
-          icon: const Icon(Icons.close),
-          onPressed: () {
-            print('holiiiis');
-          },
-        )),
-      ),
+          appBar: AppBar(
+            title: const Text('Scaffold'),
+          ),
+          body: const Center(),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print('add');
+              },
+              child: const Icon(Icons.add)),
+          drawer: Drawer(),
+          endDrawer: Drawer(),
+          backgroundColor: Color.fromARGB(255, 41, 41, 41)),
     );
   }
 }
