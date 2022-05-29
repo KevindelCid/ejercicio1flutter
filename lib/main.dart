64f1752b82aa586ac99d1618fa5651fd85317979
Kevin del Cid <danieldelcidf@gmail.com>
2022-05-29 09:05:36 -0600

    ejempl0 4 aqui hice uso de navbar o appBar como se le llama aquí puse en practica anteriores conocimientos y quedó bien mamalón

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Scaffold'),
-          ),
-          body: const Center(),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                print('add');
-              },
-              child: const Icon(Icons.add)),
-          drawer: Drawer(),
-          endDrawer: Drawer(),
-          backgroundColor: Color.fromARGB(255, 41, 41, 41)),
+        backgroundColor: const Color.fromARGB(255, 41, 41, 41),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 31, 31, 31),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.elliptical(10, 10))),
+          title: const Text('Uso del AppBar'),
+          actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.add), iconSize: 25, onPressed: _add)
+          ],
+          leading:
+              IconButton(icon: const Icon(Icons.back_hand), onPressed: _add),
+        ),
+        body: const Center(),
+      ),
     );
+  }
+
+  void _add() {
+    // ignore: avoid_print
+    print('holis');
   }
 }
