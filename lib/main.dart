7bb2df8fdc1ab82630b4211ac88f5a97a8e371cf
Kevin del Cid <danieldelcidf@gmail.com>
2022-05-29 11:48:29 -0600

    Ejemplo de uso de rows y columns para hacer tarjetas al estilo paginas que se frecuentan a escondidas

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
+  // necesito crear dos filas donde aparezcan 2 tarjetas con datos
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(), //apBar me muestra lo que en web se conoce como navbar
-      body: const Center(
-        child:  Text(
-        'Mundo! Holis :D', // muestro texto parametro obligatorio de Text
-        style: TextStyle(
-          // estilos para el texto es opcional
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.blueAccent,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tajetas con columns y Rows'),
         ),
-        
-      ), // puedo mostrar texto ...
-
-
-      )
-    
+        body: Column(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(children: <Widget>[
+                Image.network(
+                    'https://thumb-p0.xhcdn.com/a/2faOelxwBvId33kgs9a2pw/000/419/753/940_1000.gif',
+                    height: 100,
+                    width: 150),
+                const SizedBox(
+                  width: 150.0,
+                  child: AutoSizeText(
+                    'petite, morenita rica follada',
+                    style: TextStyle(fontSize: 12.0),
+                    maxLines: 3,
+                  ),
+                ),
+              ]),
+              Column(children: <Widget>[
+                Image.network(
+                    'https://static-ca-cdn.eporner.com/gallery/pT/LQ/mi9aGzWLQpT/93932-jillian-janson-anal.gif',
+                    height: 100,
+                    width: 150),
+                const SizedBox(
+                  width: 150.0,
+                  child: AutoSizeText(
+                    'tremendo anal a la mami jillian janson, me encanta ese culo hermoso',
+                    style: TextStyle(fontSize: 12.0),
+                    maxLines: 3,
+                  ),
+                ),
+              ]),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(children: <Widget>[
+                Image.network(
+                    'http://ist5-1.filesor.com/pimpandhost.com/1/4/6/0/146081/6/q/1/F/6q1FD/Mandy%20Muse%20-%2003.gif',
+                    height: 100,
+                    width: 150),
+                const SizedBox(
+                  width: 150.0,
+                  child: AutoSizeText(
+                    'Anal al hermoso culazo de mandy muse, qué rico!',
+                    style: TextStyle(fontSize: 12.0),
+                    maxLines: 3,
+                  ),
+                ),
+              ]),
+              Column(children: <Widget>[
+                Image.network(
+                    'https://realbooru.com//images/6b/07/6b07268a161bf8257c32e03746c91141.gif',
+                    height: 100,
+                    width: 150),
+                const SizedBox(
+                  width: 150.0,
+                  child: AutoSizeText(
+                    'Qué envidia me da ese negro! me encanta jewelz blu ',
+                    style: TextStyle(fontSize: 12.0),
+                    maxLines: 3,
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
