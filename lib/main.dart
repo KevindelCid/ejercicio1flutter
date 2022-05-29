daa724f1923fa354f763371fc41ebe60d6b7b8ab
Kevin del Cid <danieldelcidf@gmail.com>
2022-05-29 15:00:15 -0600

    uso de listview esto se usa para mostrar con scroll en pantalla listados probablemente cargados desde una api

--- lib/main.dart
+++ lib/main.dart
@@ -7,82 +7,78 @@ class MyApp extends StatelessWidget {
   @override
   // necesito crear dos filas donde aparezcan 2 tarjetas con datos
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
+    return MaterialApp(title: 'Material App', home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  List<String> names = [
+    "Berenice",
+    "Daniel",
+    "Nawja",
+    "Kevin",
+    "Mandy",
+    "Jewelz",
+    "Daisy",
+    "Fernanda"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Tajetas con columns y Rows'),
+          title: const Text('Uso de ListView'),
         ),
-        body: Column(children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(children: <Widget>[
-                Image.network(
-                    'https://thumb-p0.xhcdn.com/a/2faOelxwBvId33kgs9a2pw/000/419/753/940_1000.gif',
-                    height: 100,
-                    width: 150),
-                const SizedBox(
-                  width: 150.0,
-                  child: AutoSizeText(
-                    'petite, morenita rica follada',
-                    style: TextStyle(fontSize: 12.0),
-                    maxLines: 3,
-                  ),
-                ),
-              ]),
-              Column(children: <Widget>[
-                Image.network(
-                    'https://static-ca-cdn.eporner.com/gallery/pT/LQ/mi9aGzWLQpT/93932-jillian-janson-anal.gif',
-                    height: 100,
-                    width: 150),
-                const SizedBox(
-                  width: 150.0,
-                  child: AutoSizeText(
-                    'tremendo anal a la mami jillian janson, me encanta ese culo hermoso',
-                    style: TextStyle(fontSize: 12.0),
-                    maxLines: 3,
-                  ),
-                ),
-              ]),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(children: <Widget>[
-                Image.network(
-                    'http://ist5-1.filesor.com/pimpandhost.com/1/4/6/0/146081/6/q/1/F/6q1FD/Mandy%20Muse%20-%2003.gif',
-                    height: 100,
-                    width: 150),
-                const SizedBox(
-                  width: 150.0,
-                  child: AutoSizeText(
-                    'Anal al hermoso culazo de mandy muse, qué rico!',
-                    style: TextStyle(fontSize: 12.0),
-                    maxLines: 3,
-                  ),
-                ),
-              ]),
-              Column(children: <Widget>[
-                Image.network(
-                    'https://realbooru.com//images/6b/07/6b07268a161bf8257c32e03746c91141.gif',
-                    height: 100,
-                    width: 150),
-                const SizedBox(
-                  width: 150.0,
-                  child: AutoSizeText(
-                    'Qué envidia me da ese negro! me encanta jewelz blu ',
-                    style: TextStyle(fontSize: 12.0),
-                    maxLines: 3,
-                  ),
-                ),
-              ]),
-            ],
-          ),
-        ]),
-      ),
-    );
+        body: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (BuildContext context, int index) {
+            final name = names[index];
+            return ListTile(
+                title: Text(name), leading: const Icon(Icons.person));
+
+            // Card(child: Text(name));
+          },
+        )
+
+        // ListView(children: <Widget>[
+        //   Column(
+        //       crossAxisAlignment: CrossAxisAlignment.stretch,
+        //       children: <Widget>[
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //           children: <Widget>[
+        //             Column(children: <Widget>[
+        //               Image.network(
+        //                   'http://ist5-1.filesor.com/pimpandhost.com/1/4/6/0/146081/6/q/1/F/6q1FD/Mandy%20Muse%20-%2003.gif',
+        //                   height: 100,
+        //                   width: 150),
+        //               const SizedBox(
+        //                 width: 150.0,
+        //                 child: AutoSizeText(
+        //                   'Anal al hermoso culazo de mandy muse, qué rico!',
+        //                   style: TextStyle(fontSize: 12.0),
+        //                   maxLines: 3,
+        //                 ),
+        //               ),
+        //             ]),
+        //             Column(children: <Widget>[
+        //               Image.network(
+        //                   'https://i0.wp.com/lagacetauncut.com/medios/2020/05/jewelz-blu-nubiles-films-00.jpg?fit=768%2C432&ssl=1',
+        //                   height: 100,
+        //                   width: 150),
+        //               const SizedBox(
+        //                 width: 150.0,
+        //                 child: AutoSizeText(
+        //                   'jewelz blu ',
+        //                   style: TextStyle(fontSize: 12.0),
+        //                   maxLines: 3,
+        //                 ),
+        //               ),
+        //             ]),
+        //           ],
+        //         ),
+        //       ]),
+        // ]),
+        );
   }
 }
