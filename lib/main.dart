d2da7e15290329df0d56789265aaed50bedbcb5d
Kevin del Cid <danieldelcidf@gmail.com>
2022-05-29 22:57:07 -0600

    ejercicio uso del statefulWidget barra de progreso, switch setState() async  app dinamicas

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,85 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
+    return MaterialApp(title: 'Material App', home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String texto = "Esto esta hecho con statefulWidget";
+  int cont = 0;
+  double progressIndicator = 0.0;
+  bool switchVal = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(), //apBar me muestra lo que en web se conoce como navbar
-      body: const Center(
-        child:  Text(
-        'Mundo! Holis :D', // muestro texto parametro obligatorio de Text
-        style: TextStyle(
-          // estilos para el texto es opcional
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.blueAccent,
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text("$texto $cont segundos"),
+            LinearProgressIndicator(
+              value: progressIndicator,
+            ),
+            Switch(
+              value: switchVal,
+              onChanged: (value) {
+                setState(() {
+                  switchVal = value;
+                });
+              },
+            ),
+          ],
         ),
-        
-      ), // puedo mostrar texto ...
-
-
-      )
-    
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeText,
+        child: const Icon(Icons.change_circle),
+      ),
     );
+  }
+
+  void contador() {
+    setState(() {
+      cont += 1;
+      progressIndicator += 0.01;
+    });
+  }
+
+  void changeText() {
+    setState(() {
+      if (texto == "Mandy muse tiene el mejor culo del porno") {
+        texto = "Jewelz blu tiene unas tetas hermosas";
+      } else {
+        texto = "Mandy muse tiene el mejor culo del porno";
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(milliseconds: 1000), (value) {
+      contador();
+      changeText();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
