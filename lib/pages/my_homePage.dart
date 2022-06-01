4325d47be20f7b249d19348b72dc969ab7042c84
Kevin del Cid <danieldelcidf@gmail.com>
2022-05-31 22:04:12 -0600

    enviando datos desde un TextField sin widgetForm

--- lib/pages/my_homePage.dart
+++ lib/pages/my_homePage.dart
@@ -3,35 +3,68 @@
 import 'package:ejemplo1/pages/second_page.dart';
 import 'package:flutter/material.dart';
 
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   String name = "kevin";
+
+  late TextEditingController nameTextController;
+
+  late TextEditingController passTextController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Route'),
+        title: const Text('Login'),
       ),
-      body: Center(
-        child: GestureDetector(
-          onLongPress:  () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondPage(name: name)),
-            );
-          },
-          child:   RaisedButton(
-          child: Text('Open route'),
-          
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondPage(name: name)),
-            );
-          },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration:
+                  const InputDecoration(labelText: 'Ingrese su usuario'),
+              controller: nameTextController,
+            ),
+            TextField(
+              decoration:
+                  const InputDecoration(labelText: 'Ingrese su contraseña'),
+              controller: passTextController,
+            ),
+            RaisedButton(
+              child: const Text('Enviar datos'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SecondPage(
+                          name: nameTextController.text.toString(),
+                          pass: passTextController.text.toString())),
+                );
+              },
+            ),
+          ],
         ),
-        )
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    nameTextController = TextEditingController();
+
+    passTextController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameTextController.dispose();
+    passTextController.dispose();
   }
 }
--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -2,29 +2,35 @@ import 'package:ejemplo1/pages/pagina3.dart';
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key, required this.name}) : super(key: key);
+  const SecondPage({Key? key, required this.name, required this.pass})
+      : super(key: key);
 
-
-final String name;
+  final String name;
+  final String pass;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hola $name"),
+        title: Text(pass),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigator.pop(context);
+      body: Column(
+        children: [
+          Center(
+            child: RaisedButton(
+              onPressed: () {
+                // Navigator.pop(context);
 
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => PageTre(name: name)),
-            );
-          },
-          child: Text('Listo, vamos a la siguiente Pagina!'),
-        ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PageTre(name: name)),
+                );
+              },
+              child: Text('Listo, vamos a la siguiente Pagina!'),
+            ),
+          ),
+          Text("este es el nombre: $name y esta la contra: $pass"),
+        ],
       ),
     );
   }
